c85a74bdcba8f46a143d4fdc082ae3df367aeb6a
tbrixen <tbrixen@gmail.com>
2017-12-30 11:27:54 +0100

    Probably solved challenge 08

--- 08detectAesInEcbMode.py
+++ 08detectAesInEcbMode.py
@@ -1,11 +1,11 @@
-from lib.basic import canReadNextBytes
+from lib.basic import canReadNextBytes, decrypt_aes_ecb
 from bitstring import ConstBitStream
 from collections import defaultdict
 
 def ecb_score(data):
     counter = defaultdict(int)
     while canReadNextBytes(data, 16):
-        nextBytes = data.read(16*8)
+        nextBytes = data.read(16 * 8)
         counter[nextBytes] += 1
 
     total = 0
@@ -13,13 +13,30 @@ def ecb_score(data):
         total += 2 ** count
     return total
 
+def find_most_probable_ecb_encrypted(data):
+    data_to_score_map = {}
+    for encrypted_value in data:
+        score = ecb_score(encrypted_value)
+        data_to_score_map[encrypted_value] = score
+
+    bestScore = 0
+    bestData = ConstBitStream()
+    for encrypted_value, count in data_to_score_map.items():
+        if count > bestScore:
+            bestScore = count
+            bestData = encrypted_value
+
+    return bestData
+
 def main():
-    scores = {}
+    data = list()
     f = open("08detectAesInEcbMode.txt")
     for line in f.read().split('\n'):
-        score = ecb_score(ConstBitStream(hex=line))
-        scores[line] = score
+        data.append(ConstBitStream(hex=line))
     f.close()
+
+    mostProbable = find_most_probable_ecb_encrypted(data)
+    print mostProbable.hex
 
 if __name__ == "__main__":
     main()
