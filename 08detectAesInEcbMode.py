14a819f85fdf5f50f58facbd1e9d9addef6d3fd6
tbrixen <tbrixen@gmail.com>
2017-12-30 11:12:30 +0100

    Add scoring

--- 08detectAesInEcbMode.py
+++ 08detectAesInEcbMode.py
@@ -1,21 +1,25 @@
-from lib.basic import canReadNextBytes, decrypt_aes_ecb
+from lib.basic import canReadNextBytes
 from bitstring import ConstBitStream
+from collections import defaultdict
 
 def ecb_score(data):
-    return 42
+    counter = defaultdict(int)
+    while canReadNextBytes(data, 16):
+        nextBytes = data.read(16*8)
+        counter[nextBytes] += 1
+
+    total = 0
+    for count in counter.itervalues():
+        total += 2 ** count
+    return total
 
 def main():
-    key = ConstBitStream(bytes="YELLOW SUBMARINE")
-
     scores = {}
     f = open("08detectAesInEcbMode.txt")
     for line in f.read().split('\n'):
-        print line
         score = ecb_score(ConstBitStream(hex=line))
         scores[line] = score
     f.close()
-
-    print scores
 
 if __name__ == "__main__":
     main()
